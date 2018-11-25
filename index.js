5d473bc655a613f98ed89330334a997867331ffb
Shannon Nabors <shannon.nabors@gmail.com>
2018-11-25 18:42:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function createRock(x) {
 function endGame() {
   clearInterval(gameInterval);
   for(var i = 0; i < ROCKS.length; i++) {
-    GAME.removeChild(ROCKS[i]);
+    ROCKS[i].remove;
   }
   window.removeEventListener('keydown', moveDodger);
   window.alert("YOU LOSE!");
