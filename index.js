e6482a5a981f3bc55cdb13c8baadc24e8ea9aeb8
Shannon Nabors <shannon.nabors@gmail.com>
2018-11-25 18:41:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -76,6 +76,7 @@ function endGame() {
 function moveDodger(e) {
   if(e.which === LEFT_ARROW) {
     e.stopPropagation();
+    e.preventDefault();
     moveDodgerLeft();
   }
   if(e.which === RIGHT_ARROW) {
